12920c515b6ff1a895c1ba6cd55d893a90f653f0
CeriMey <81415598+CeriMey@users.noreply.github.com>
2025-09-14 13:16:20 +0200

    add shared st_common utilities

--- st0903/st0903.h
+++ st0903/st0903.h
@@ -1,27 +1,28 @@
 #pragma once
 
 #include "klv.h"
+#include "st_common.h"
 
 namespace misb {
 namespace st0903 {
 
-// Universal Labels for a subset of MISB ST0903 tags
-extern const UL VMTI_TARGET_ID;
-extern const UL VMTI_DETECTION_STATUS;
-extern const UL VMTI_DETECTION_PROBABILITY;
-extern const UL VMTI_TRACKER_ID;
-extern const UL VMTI_CLASS_ID;
-extern const UL VMTI_TOTAL_TARGETS_DETECTED;
-extern const UL VMTI_NUM_TARGETS_REPORTED;
-extern const UL VMTI_FRAME_WIDTH;
-extern const UL VMTI_FRAME_HEIGHT;
-extern const UL VMTI_SOURCE_SENSOR;
-extern const UL VMTI_HORIZONTAL_FOV;
-extern const UL VMTI_VERTICAL_FOV;
-extern const UL VMTI_MIIS_ID;
-extern const UL VMTI_CENTROID_ROW;
-extern const UL VMTI_CENTROID_COL;
-extern const UL VMTI_ALGORITHM_ID;
+constexpr uint8_t ST_ID = 0x03;
+constexpr UL VMTI_TARGET_ID        = make_st_ul(ST_ID, 0x00);
+constexpr UL VMTI_DETECTION_STATUS = make_st_ul(ST_ID, 0x01);
+constexpr UL VMTI_DETECTION_PROBABILITY = make_st_ul(ST_ID, 0x02);
+constexpr UL VMTI_TRACKER_ID       = make_st_ul(ST_ID, 0x03);
+constexpr UL VMTI_CLASS_ID         = make_st_ul(ST_ID, 0x04);
+constexpr UL VMTI_TOTAL_TARGETS_DETECTED = make_st_ul(ST_ID, 0x05);
+constexpr UL VMTI_NUM_TARGETS_REPORTED  = make_st_ul(ST_ID, 0x06);
+constexpr UL VMTI_FRAME_WIDTH      = make_st_ul(ST_ID, 0x07);
+constexpr UL VMTI_FRAME_HEIGHT     = make_st_ul(ST_ID, 0x08);
+constexpr UL VMTI_SOURCE_SENSOR    = make_st_ul(ST_ID, 0x09);
+constexpr UL VMTI_HORIZONTAL_FOV   = make_st_ul(ST_ID, 0x0A);
+constexpr UL VMTI_VERTICAL_FOV     = make_st_ul(ST_ID, 0x0B);
+constexpr UL VMTI_MIIS_ID          = make_st_ul(ST_ID, 0x0C);
+constexpr UL VMTI_CENTROID_ROW     = make_st_ul(ST_ID, 0x0D);
+constexpr UL VMTI_CENTROID_COL     = make_st_ul(ST_ID, 0x0E);
+constexpr UL VMTI_ALGORITHM_ID     = make_st_ul(ST_ID, 0x0F);
 
 // Register encode/decode lambdas for the above ULs
 void register_st0903(KLVRegistry& reg);
