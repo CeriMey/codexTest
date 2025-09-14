9779804891fb6383dbd70d516e9013a01ea873a0
CeriMey <81415598+CeriMey@users.noreply.github.com>
2025-09-14 11:18:52 +0200

    Add more ST0903 ULs for sensor and target tags

--- st0903/st0903.h
+++ st0903/st0903.h
@@ -11,6 +11,17 @@ extern const UL VMTI_DETECTION_STATUS;
 extern const UL VMTI_DETECTION_PROBABILITY;
 extern const UL VMTI_TRACKER_ID;
 extern const UL VMTI_CLASS_ID;
+extern const UL VMTI_TOTAL_TARGETS_DETECTED;
+extern const UL VMTI_NUM_TARGETS_REPORTED;
+extern const UL VMTI_FRAME_WIDTH;
+extern const UL VMTI_FRAME_HEIGHT;
+extern const UL VMTI_SOURCE_SENSOR;
+extern const UL VMTI_HORIZONTAL_FOV;
+extern const UL VMTI_VERTICAL_FOV;
+extern const UL VMTI_MIIS_ID;
+extern const UL VMTI_CENTROID_ROW;
+extern const UL VMTI_CENTROID_COL;
+extern const UL VMTI_ALGORITHM_ID;
 
 // Register encode/decode lambdas for the above ULs
 void register_st0903(KLVRegistry& reg);
